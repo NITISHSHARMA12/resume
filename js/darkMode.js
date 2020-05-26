99aade5652c5b3131544211eabf19400662a5e42
nitishsharma <nitishsharma@plutonic.co.in>
2020-05-26 17:34:51 +0530

    dark mode improved

--- js/darkMode.js
+++ js/darkMode.js
@@ -2,6 +2,7 @@ function myFunction() {
     var element = document.getElementById("darkmode");
     $("div").toggleClass("darkmode");
     $("li").toggleClass("darkmodeList");
+    $(".card-title").toggleClass("card-title-dark");
     console.log(element)
 
 }
